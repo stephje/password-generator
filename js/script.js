94bbe8e7bf9b9d57c9f8954a81629c47c9133f11
stephje <s.jenkins3018@gmail.com>
2021-03-23 05:50:51 +0000

    Add function to create character pool

--- js/script.js
+++ js/script.js
@@ -8,24 +8,34 @@ function writePassword() {
 
   function generatePassword() {
 
+    const alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    const availableNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    const specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '\\', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '}', '~']
+
+    var passwordLength = askForPasswordLength();
+    var characterTypes = checkCharacterTypes();
+    var characterPool = createCharacterPool();
+
+    //TESTING DELETE THIS AT THE END
+    console.log(passwordLength);
+    console.log(characterTypes);
+    console.log(characterPool);
+
     //prompt user for password length
     //validate user input - if invalid then alert and prompt again, if valid then return
-    var passwordLength = (function askForPasswordLength() {
+    function askForPasswordLength() {
       var userInput = prompt("Choose a password length between 8 and 128 characters:");
       if (userInput < 8 || userInput > 128 || isFinite(userInput) !== true) {
         window.alert("You must enter a number between 8 and 128. Please try again!");
         askForPasswordLength()
       }
       return userInput;
-    })();
-
-    //TESTING DELETE THIS AT THE END
-    console.log(passwordLength)
+    }
 
     //ask user the questions sored in array "questions"
     //store results in array "answers"
-
-    var characterTypes = (function checkCharacterTypes() {
+    function checkCharacterTypes() {
       const questions = ["Include lowercase characters? Y/N", "Include uppercase characters? Y/N", "Include numeric characters? Y/N", "Include special characters? Y/N"]
       var answers = [];
       var i;
@@ -34,23 +44,27 @@ function writePassword() {
           answers.push(answer)
       }
       return answers;
-    })();
-
-    //TESTING DELETE THIS AT THE END
-    console.log(characterTypes);
-
-    //generate a random number up to a specified value
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
     }
-    
-      const alphabetLower = "abcdefghijklmnopqrstuvwxyz";
-      const alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      const availableNumbers = "0123456789";
-      const specialCharacters = "\!\#\$\%\&\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\}\~";
-      const options = [alphabetLower, alphabetUpper, availableNumbers, specialCharacters];
-      var randomString = "";
-    
+
+    function createCharacterPool() {
+      var nominatedCharacters = [];
+      switch (true) {
+        case (characterTypes[0]):
+          nominatedCharacters = nominatedCharacters.concat(alphabetLower);
+        case (characterTypes[1]): 
+          nominatedCharacters = nominatedCharacters.concat(alphabetUpper);
+        case (characterTypes[2]):
+          nominatedCharacters = nominatedCharacters.concat(availableNumbers);
+        case (characterTypes[3]):
+          nominatedCharacters = nominatedCharacters.concat(specialChars);
+          break;
+        default: 
+          window.alert("You must select at least one character type. Please try again!");
+          checkCharacterTypes();
+      }
+      return nominatedCharacters;
+    }
+
   }
   passwordText.value = password;
 }
