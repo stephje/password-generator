3048a33fbbd9844b8b48201adef425ba2b8aac6f
stephje <s.jenkins3018@gmail.com>
2021-03-24 08:02:27 +0000

    Add function to generate random character

--- js/script.js
+++ js/script.js
@@ -7,12 +7,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   function generatePassword() {
-
+    //Constants containing different character types that can be used to generate the passwords
     const alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     const alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     const availableNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     const specialChars = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '\\', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '}', '~']
 
+    //variables that will be used by the generatePassword function
     var passwordLength = askForPasswordLength();
     var characterTypes = checkCharacterTypes();
     var characterPool = createCharacterPool();
@@ -22,8 +23,8 @@ function writePassword() {
     console.log(characterTypes);
     console.log(characterPool);
 
-    //prompt user for password length
-    //validate user input - if invalid then alert and prompt again, if valid then return
+    //Function that prompts user for password length
+    //Validates user input - if invalid, then an alert is generated and the user is prompted again, if valid then return the user input
     function askForPasswordLength() {
       var userInput = prompt("Choose a password length between 8 and 128 characters:");
       if (userInput < 8 || userInput > 128 || isFinite(userInput) !== true) {
@@ -33,8 +34,9 @@ function writePassword() {
       return userInput;
     }
 
-    //ask user the questions sored in array "questions"
-    //store results in array "answers"
+    //Function that asks the user the set of questions stored in array "questions"
+    //Stores the resulting boolean values in an array called "answers"
+    //Returns the array called "answers"
     function checkCharacterTypes() {
       const questions = ["Include lowercase characters? Y/N", "Include uppercase characters? Y/N", "Include numeric characters? Y/N", "Include special characters? Y/N"]
       var answers = [];
@@ -46,24 +48,61 @@ function writePassword() {
       return answers;
     }
 
+    //Function that generates a random number
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    //Function that creates an array of characters based on the character types the user selected when function checkCharacterTypes was executed
+    //Returns the combined array
     function createCharacterPool() {
       var nominatedCharacters = [];
+      var passwordCharacters = [];
+
+      function getRandomCharacter() {
+        var randomInt = getRandomInt(arrayName.length);
+        var randomLetter = arrayName[randomInt];
+        passwordCharacters.push(randomLetter);
+        console.log(passwordCharacters);
+      }
+
       switch (true) {
+
         case (characterTypes[0]):
           nominatedCharacters = nominatedCharacters.concat(alphabetLower);
+          var arrayName = alphabetLower;
+          getRandomCharacter();
+
         case (characterTypes[1]): 
           nominatedCharacters = nominatedCharacters.concat(alphabetUpper);
+          var arrayName = alphabetUpper;
+          getRandomCharacter();
+
         case (characterTypes[2]):
           nominatedCharacters = nominatedCharacters.concat(availableNumbers);
+          var arrayName = availableNumbers;
+          getRandomCharacter();
+
         case (characterTypes[3]):
           nominatedCharacters = nominatedCharacters.concat(specialChars);
+          var arrayName = specialChars;
+          getRandomCharacter();
+
           break;
         default: 
           window.alert("You must select at least one character type. Please try again!");
           checkCharacterTypes();
       }
+      console.log(passwordCharacters);
       return nominatedCharacters;
     }
+
+    //Password must contain at least one character of each nominated type
+    //This function selects one character at random from the array for each nominated type and stores it in a new array
+    function getOneOfEachCharacterType() {
+      
+    }
+
 
   }
   passwordText.value = password;
